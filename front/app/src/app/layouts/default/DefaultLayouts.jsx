3e670bd49c624ab8533245799a13c6cf9da30ed9
shj7157 <ana3630@naver.com>
2026-05-07 16:44:46 +0900

    fix : admin공통 layout css수정, 디폴트레이아웃 수정 / 현진

--- front/app/src/app/layouts/default/DefaultLayouts.jsx
+++ front/app/src/app/layouts/default/DefaultLayouts.jsx
@@ -1,74 +1,83 @@
-import React from "react";
-import styled from "styled-components";
-import Header from "./../header/Header";
-import Nav from "./../nav/Nav";
-import Footer from "./../footer/Footer";
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import styled from 'styled-components';
+import Header from './../header/Header';
+import Nav from './../nav/Nav';
+import Footer from './../footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 const LayoutContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	min-height: 100vh;
-	width: 100%;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100%;
 `;
 
 const HeaderArea = styled.header`
-	height: 70px;
-	background-color: #f4efe6;
-	color: #333;
-	flex-shrink: 0;
+  height: 70px;
+  background-color: #f4efe6;
+  color: #333;
+  flex-shrink: 0;
 `;
 
 const BodyArea = styled.div`
-	flex: 1;
-	display: flex;
-	flex-direction: row;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
 `;
 
 const NavArea = styled.nav`
-	width: 250px;
-	background-color: #a8b89f;
-	color: #fff;
-	flex-shrink: 0;
-	overflow-y: auto;
+  width: 250px;
+  background-color: #a8b89f;
+  color: #fff;
+  flex-shrink: 0;
+  height: 85.3vh;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 3px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: rgba(255, 255, 255, 0.3);
+    border-radius: 10px;
+  }
 `;
 
 const MainArea = styled.main`
-	flex: 1;
-	padding: 20px;
-	background-color: #f4efe6;
-	color: #333;
-	overflow-y: auto;
+  flex: 1;
+  height: 85.3vh;
+
+  background-color: #f4efe6;
+  color: #333;
 `;
 
 const FooterArea = styled.footer`
-	height: 70px;
-	background-color: #f4efe6;
-	color: #333;
-	flex-shrink: 0;
+  height: 70px;
+  background-color: #f4efe6;
+  color: #333;
+  flex-shrink: 0;
 `;
 
 function DefaultLayouts() {
-	return (
-		<LayoutContainer>
-			<HeaderArea>
-				<Header />
-			</HeaderArea>
+  return (
+    <LayoutContainer>
+      <HeaderArea>
+        <Header />
+      </HeaderArea>
 
-			<BodyArea>
-				<NavArea>
-					<Nav />
-				</NavArea>
-				<MainArea>
-					<Outlet />
-				</MainArea>
-			</BodyArea>
+      <BodyArea>
+        <NavArea>
+          <Nav />
+        </NavArea>
+        <MainArea>
+          <Outlet />
+        </MainArea>
+      </BodyArea>
 
-			<FooterArea>
-				<Footer />
-			</FooterArea>
-		</LayoutContainer>
-	);
+      <FooterArea>
+        <Footer />
+      </FooterArea>
+    </LayoutContainer>
+  );
 }
 
 export default DefaultLayouts;
