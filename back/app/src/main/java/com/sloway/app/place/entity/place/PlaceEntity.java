4104de79a6c613d3096fef3b5a4b4f4b3de3dd81
shj7157 <ana3630@naver.com>
2026-05-15 01:15:06 +0900

    fix : baseEntity누락된 곳들 추가 / 현진

--- back/app/src/main/java/com/sloway/app/place/entity/place/PlaceEntity.java
+++ back/app/src/main/java/com/sloway/app/place/entity/place/PlaceEntity.java
@@ -53,7 +53,6 @@ public class PlaceEntity extends BaseEntity {
     private int viewCnt = 0;
 
     // 이미지 연관관계 추가
-    // cascade = CascadeType.ALL: Place 저장 시 이미지도 같이 저장
     @Builder.Default
     @OneToMany(mappedBy = "placeEntity", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ImgPlaceEntity> images = new ArrayList<>();
