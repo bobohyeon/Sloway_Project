301677c6656a80e9292520f27a1dcda0a0be4253
DESKTOP-7BF8T50\user1 <bohyeonkk6161@gmail.com>
2026-05-19 15:19:57 +0900

    feat : recentViewed 구현

---
+++ back/app/src/main/java/com/sloway/app/recent/viewed/controller/RecentViewedController.java
@@ -0,0 +1,60 @@
+package com.sloway.app.recent.viewed.controller;
+
+import com.sloway.app.auth.user.CustomUserDetails;
+import com.sloway.app.recent.viewed.dto.response.RecentViewedResDto;
+import com.sloway.app.recent.viewed.service.RecentViewedService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/recent/viewed")
+@RequiredArgsConstructor
+@RestController
+public class RecentViewedController {
+
+    private final RecentViewedService recentViewedService;
+
+    @PostMapping
+    public ResponseEntity<Void> save(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestParam Long placeNo
+    ){
+        recentViewedService.save(userDetails.getMemberNo(), placeNo);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RecentViewedResDto>> findAll(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ){
+        List<RecentViewedResDto> dtoList = recentViewedService.findAll(userDetails.getMemberNo());
+        return ResponseEntity.ok(dtoList);
+    }
+
+    @DeleteMapping("/{no}")
+    public ResponseEntity<Void> deleteOne(
+            @PathVariable Long no,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ){
+        recentViewedService.deleteOne(no, userDetails.getMemberNo());
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAll(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ){
+        recentViewedService.deleteAll(userDetails.getMemberNo());
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
+}
