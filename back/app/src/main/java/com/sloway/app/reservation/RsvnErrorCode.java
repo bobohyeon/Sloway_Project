403483e9a6dd4b4d95e8f8dac3441c07249b86db
DESKTOP-7BF8T50\user1 <bohyeonkk6161@gmail.com>
2026-05-20 18:57:48 +0900

    feat : review 구현

--- back/app/src/main/java/com/sloway/app/reservation/RsvnErrorCode.java
+++ back/app/src/main/java/com/sloway/app/reservation/RsvnErrorCode.java
@@ -13,9 +13,11 @@ public enum RsvnErrorCode implements ErrorCode {
     PLACE_NOT_FOUND(HttpStatus.NOT_FOUND, "공간을 찾을 수 없습니다."),
     RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 예약을 찾을 수 없습니다."),
     BLACKOUT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 이용불가 설정을 찾을 수 없습니다."),
+
     ALREADY_CANCELLED(HttpStatus.BAD_REQUEST, "이미 취소된 예약입니다."),
     BLACKOUT_CONFLICT(HttpStatus.BAD_REQUEST, "이미 예약 불가 설정된 기간입니다."),
     INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "체크아웃 날짜는 체크인 날짜보다 이후여야 합니다."),
+
     UNAUTHORIZED_ACCESS(HttpStatus.FORBIDDEN, "해당 기능에 대한 접근권한이 없습니다.");
 
     private final HttpStatus status;
