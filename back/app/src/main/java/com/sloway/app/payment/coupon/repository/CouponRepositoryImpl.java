664d12dad5ad5828bad4df18c6929d8001c909e0
blackbean9081 <blackbean9081@gmail.com>
2026-05-20 17:26:55 +0900

    feat : Point , coupon 조회 기능 구현 by 우영

--- back/app/src/main/java/com/sloway/app/payment/coupon/repository/CouponRepositoryImpl.java
+++ back/app/src/main/java/com/sloway/app/payment/coupon/repository/CouponRepositoryImpl.java
@@ -1,13 +1,31 @@
 package com.sloway.app.payment.coupon.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sloway.app.payment.coupon.common.CouponStatus;
+import com.sloway.app.payment.coupon.entity.CouponEntity;
+import com.sloway.app.payment.coupon.entity.QCouponEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class CouponRepositoryImpl implements CouponRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
+    private static final QCouponEntity qCouponEntity = QCouponEntity.couponEntity;
+
+    @Override
+    public List<CouponEntity> findByMemberAndStatus(Long memberNo, CouponStatus status) {
+        List<CouponEntity> couponList = jpaQueryFactory
+                .selectFrom(qCouponEntity)
+                .where(
+                        qCouponEntity.memberNo.no.eq(memberNo),
+                        qCouponEntity.status.eq(status)
+                )
+                .fetch();
+        return couponList;
+    }
 
 }
