909b5de760495bd43a4850e22632c2579493767c
ChuHyunJong <chuhyunjong>
2026-05-27 17:04:35 +0900

    fix: 공지사항 물리삭제 -> 논리삭제 변경

--- back/app/src/main/java/com/sloway/app/notice/repository/NoticeRepository.java
+++ back/app/src/main/java/com/sloway/app/notice/repository/NoticeRepository.java
@@ -3,5 +3,11 @@ package com.sloway.app.notice.repository;
 import com.sloway.app.notice.entity.NoticeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface NoticeRepository extends JpaRepository<NoticeEntity , Long>, NoticeRepositoryCustom {
+    List<NoticeEntity> findAllByIdInAndDelYn(List<Long> ids, String n);
+
+    Optional<NoticeEntity> findByIdAndDelYn(Long id, String n);
 }
